0573b18096826629fffb193bea215a1839a606dd
christostsekouronas <tsekchr@gmail.com>
2021-08-03 16:11:04 +0300

    trying

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ allConfigKeys = configLoader.loadConfig()
 data = dataLibrary.loadTest(allConfigKeys["datasetfile"])
 
 # Print grade based on surname
-#
+import pandas as pd
+
+df = pd.read_csv('data-utf8.csv')
+
+print(df.head(10))
 
 
-
-if allConfigKeys["reportOnRead"]:
-    print(data)
-else:
-    print("No report is configured")
+#if allConfigKeys["reportOnRead"]:
+    #print(data)
+#else:
+    #print("No report is configured")
 
